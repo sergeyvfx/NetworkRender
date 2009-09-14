7250cb389a42fd437a1aab2ec1df02fdc2afd34b
Sergey I. Sharybin <g.ulairi@gmail.com>
2009-09-14 17:36:25 +0600

    * bpymodules/NetworkRender/Configurer.py: Added some comments.

--- bpymodules/NetworkRender/Configurer.py
+++ bpymodules/NetworkRender/Configurer.py
@@ -9,9 +9,17 @@ import Blender
 from Blender import Draw, Registry
 
 class Configurer():
+	"""
+	NetworkRendering configurer singleton
+	"""
+
 	__instance = None
 
 	class __impl():
+		"""
+		Instance for configurer singleton
+		"""
+
 		def __init__(self):
 			self.variables = {}
 			self.initExtensions()
@@ -19,10 +27,17 @@ class Configurer():
 			self.readRegistry()
 
 		def initExtensions(self):
+			"""
+			Initialize list of extensions
+			"""
 			self.extensions = {'jpeg': Blender.Scene.Render.JPEG, \
 							'png': Blender.Scene.Render.PNG}
 
 		def initVariables(self):
+			"""
+			Initialize variables dictionary with their default values
+			"""
+
 			self.declareVariable('ServerPort', 8080)
 			self.declareVariable('ServerBCast', '255.255.255.255')
 			self.declareVariable('ServerBCastInterval', 5)
@@ -34,12 +49,24 @@ class Configurer():
 			self.declareVariable('ImageType', Blender.Scene.Render.JPEG)
 
 		def declareVariable(self, name, value):
+			"""
+			Declare variable in dictionary
+			@param name: name of variable to declare
+			@param value: variable's value
+			"""
 			self.setDrawValue(name, Draw.Create(value))
 
 		def setDrawValue(self, name, value):
+			"""
+			Set button value to dictionary
+			@param name: name of variable
+			"""
 			self.variables[name] = value
 
 		def readRegistry(self):
+			"""
+			Read variables' values from registry to dictionary
+			"""
 			dict = Registry.GetKey('NetworkRender', False)
 			if dict:
 				try:
@@ -50,26 +77,48 @@ class Configurer():
 					self.writeRegistry()
 
 		def writeRegistry(self):
+			"""
+			Write dictionary variables' values to registry for future usage
+			"""
 			dict = {}
 			for name in self.variables:
 				dict[name] = self.variables[name].val
 			Registry.SetKey('NetworkRender', dict, False)
 
 		def get(self, name):
+			"""
+			Get variable's value
+			@param name - name of variable to get
+			"""
 			return self.variables[name].val
 
 		def set(self, name, value):
+			"""
+			Set variable's value
+			@param name: name of variable to set value of
+			@param value: new variable's value
+			"""
 			self.variables[name].val = value
 
 		def getExtenssions(self):
+			"""
+			Get dictionary of known extensions
+			"""
 			return self.extenstions
 
 		def getFileExtension(self, type):
+			"""
+			Get file extension by imageType constant
+			@param type: imageType to get extension for
+			"""
 			for x in self.extensions:
 				if  self.extensions[x] == type:
 					return x
 
 		def getMenuExtensions(self):
+			"""
+			Get string with extensions which could be used for menu title
+			"""
 			result = ''
 			for x in self.extensions:
 				if result <> '':
@@ -78,6 +127,9 @@ class Configurer():
 			return result
 
 	def __init__(self):
+		"""
+		Initialize configuration singleton
+		"""
 		if Configurer.__instance is None:
 			Configurer.__instance = Configurer.__impl()
 
@@ -90,5 +142,8 @@ class Configurer():
 		return setattr(self.__instance, attr, value)
 
 	def destroy(self):
+		"""
+		Destroy stored instance of object
+		"""
 		Configurer.__instance = None
 		del self.__dict__['_Configurer__instance']
